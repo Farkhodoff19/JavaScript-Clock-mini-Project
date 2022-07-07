8fdab6347a578e2350b08167d407bbdfea6cc699
Farkhodoff21 <farkhodoffcodes@gmail.com>
2022-07-07 16:23:40 +1400

    initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,30 @@
+
+function showTime() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    
+    
+    if(hours == 0) {
+        hours = 12;
+    }
+    
+    if (hours > 12) {
+        hours = hours - 12;  
+    }
+    
+    hours = (hours < 10) ? "0" +  hours : hours;
+    minutes = (minutes < 10) ? "0" +  minutes : minutes;
+    seconds = (seconds < 10) ? "0" +  seconds : seconds;
+    
+    var time = hours + ":" + minutes + ":" + seconds;
+    
+    document.querySelector('#clockD').innerText = time;
+    document.querySelector('#clockD').textContent = time;
+    
+    setTimeout(showTime , 1000)
+}
+
+showTime();
+
